0d3827e3f224ba407052ee950fad7fa76bcb8f9a
Gurleen Kaur <73643543+gurleenMaini@users.noreply.github.com>
2021-12-01 18:42:02 +0530

    Add files via upload
    This is my first project on python. It is basically a number guessing game where you will be given 5 chances. It will ask you to enter a number and if your number is matched correctly with the generated number then you will win the game. But if your all the guessed numbers are wrong than you will lose the game.
    Hope you all will like my game :)

---
+++ NumberGuessing.py
@@ -0,0 +1,23 @@
+import random
+print("Number Guessing Game")
+number= random.randint(1,9)
+chances= 0
+print("guess a number between(1 to 9)")
+
+while(chances<5):
+    guess = int(input("Enter your guess"))
+
+    if(guess==number):
+        print("Congratulations YOU WON!!!")
+        break
+
+    elif(guess<number):
+        print("your guessed number is lower than the generated number")
+
+    else:
+        print("your guessed number is greater than the generated number")
+
+    chances+= 1
+
+if not chances < 5:
+    print("YOU LOSE!!! The number is", number)
